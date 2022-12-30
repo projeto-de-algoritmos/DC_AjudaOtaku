fe758e4e85138083c7402f7a1212b4cbd19ae612
Felipe Agustini <felipeboccardi@hotmail.com>
2022-12-30 13:50:27 -0300

    Finaliza interface e mostra resultado de inversões
    Co-authored-by: giovannabbottino <giovannabbottino@gmail.com>

--- app/src/pages/AnimePage/index.js
+++ app/src/pages/AnimePage/index.js
@@ -5,42 +5,66 @@ import AnimeCard from '../../components/AnimeCard'
 import api from "../../services/api";
 import './style.css';
 
-
-
-function consoleCall(lista){
-  console.log(lista);
-
-}
-
+const list = [];
 export default function AnimePage() {
-  const [animes, setAnimeList] = useState([]);
+  const [otaku, setOtaku] = useState('');
+  const [animes, setAnimes] = useState([]);
+  const [animeList, setAnimeList] = useState([]);
+  function listAdd(animesList,otaku){
+    if (otaku == ''){
+      alert("nome em branco");
+    }else{
+      const titles = animesList.map(({ title }) => title);
+      list.push({array: titles,name: otaku});
+      console.log(list);
+      setAnimeList(animes);
+      setOtaku('');
+      document.getElementById("otakuname").value=""
+    }
+  }
   let navigate = useNavigate(); 
+
   const routeChange = () =>{ 
     let path = `/results`; 
-    navigate(path);
+    navigate(path, {state:{list:list}});
   }
-  
+  const handleChange = (event) => {
+    setOtaku(event.target.value);
+  };
   useEffect(() => {
     api
       .get("/api/anime")
-      .then((response) => setAnimeList(response.data))
+      .then((response) => {
+        setAnimes(response.data);
+        setAnimeList(response.data);})
       .catch((err) => {
         console.error("ops! ocorreu um erro" + err);
       });
   }, []);
 
+  if (animes.length == 0) return <div>Carregando</div>;
  
   return (
     <div className="anime">
       <header className="page-header">
-      <div className="anime-header">
-        <ReactSortable list={animes} setList={setAnimeList} className='anime-grid'>
-            {animes && animes.length > 0 && animes.map((anime, index) => (
-              <AnimeCard key={index} anime={{image: anime.image, title: anime.title}} index={index + 1}/>
-            ))}
-        </ReactSortable>
-        </div>
-        <button className="button send-button" onClick={() => consoleCall(animes)}>
+        <div className="otakuName">
+            <label>Seu nome:&nbsp;
+                <input
+                    type="text"
+                    id="otakuname"
+                    name="otaku"
+                    onChange={handleChange}
+                required/>
+            </label>
+          </div>
+          <div className="anime-header">
+                <ReactSortable list={animeList} setList={setAnimeList} className='anime-grid'>
+                    {animeList && animeList.length > 0 && animeList.map((anime, index) => (
+                        <AnimeCard key={index} anime={{image: anime.image, title: anime.title}} index={index + 1}/>
+                    ))}
+                </ReactSortable>
+            </div>
+        <button className="button send-button" onClick={() => listAdd(animeList,otaku)}>
                 Enviar Lista
         </button>
         <button className="button results-button" onClick={routeChange}>
--- app/src/pages/ResultsPage/index.js
+++ app/src/pages/ResultsPage/index.js
@@ -1,40 +1,59 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React,{ useEffect ,useState} from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import api from "../../services/api";
 import './style.css';
 
 
 export default function ResultsPage() {
+  const location = useLocation();
+  const [list] = useState(location.state.list);
 
-    let navigate = useNavigate(); 
-    const routeChange = () =>{ 
-      let path = `/`; 
-      navigate(path);
-    }
-    
-    const animes = [{"title":"Genjitsu Shugi Yuusha no Oukoku Saikenki","image":"https://cdn.myanimelist.net/images/anime/1297/118764.jpg"},
-               {"title":"Tensai Ouji no Akaji Kokka Saisei Jutsu","image":"https://cdn.myanimelist.net/images/anime/1263/119511.jpg"},
-               {"title":"Show By Rock!!","image":"https://cdn.myanimelist.net/images/anime/7/73674.jpg"},
-               {"title":"Kemono Friends","image":"https://cdn.myanimelist.net/images/anime/2/83518.jpg"},
-               {"title":"Soul Eater","image":"https://cdn.myanimelist.net/images/anime/9/7804.jpg"},
-               {"title":"Tegamibachi","image":"https://cdn.myanimelist.net/images/anime/9/71808.jpg"},
-               {"title":"Sword Art Online","image":"https://cdn.myanimelist.net/images/anime/11/39717.jpg"},
-               {"title":"Overlord","image":"https://cdn.myanimelist.net/images/anime/7/88019.jpg"},
-               {"title":"gdgd Fairies","image":"https://cdn.myanimelist.net/images/anime/8/34963.jpg"},
-               {"title":"Devidol!","image":"https://cdn.myanimelist.net/images/anime/1094/93923.jpg"},
-               {"title":"Tensei shitara Slime Datta Ken","image":"https://cdn.myanimelist.net/images/anime/1694/93337.jpg"}]
+  let navigate = useNavigate(); 
+  const routeChange = () =>{ 
+    navigate(-1);
+  }
+
+  const [response, setResponse] = useState([]);
+  useEffect(() => {
+    api
+      .post("/api/inversions", {data: list})
+      .then((response) => setResponse(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
+  if (!list || response.length == 0){
     return (
-      <div className="results-header">
-             {animes && animes.length > 0 && animes.map((anime, index) => (
-                <div className="results-card">
-                    <div className="results-content">
-                        <h1 className="results-position">{index + 'º'}</h1>
-                        <h1 className="results-title">{anime.title}</h1>
-                    </div>
-               </div>
-            ))}
+      <div className="responses-header">
+            <div>Erro, tente novamente</div>
             <button className="button back-button" onClick={routeChange}> Voltar </button>
       </div>
     );
   }
+
+  return (
+    <div className="responses-header">
+      <h1 className="ranking-text">Inversões</h1>
+      {response.data && response.data.length > 0 && response.data.map((person, index) => (
+              <div className="person-card" key={index}>
+                  <div className="person-content">
+                      <h1 className="person-text">{person.name + ' teve ' + person.inversions + ' inversões'}</h1>
+                  </div>
+              </div>
+          ))}
+          <h1 className="ranking-text">Ranking</h1>
+            {response.ranking && response.ranking.length > 0 && response.ranking.map((anime, index) => (
+              <div className="responses-card" key={index}>
+                  <div className="responses-content">
+                      <h1 className="responses-position">{index+1 + 'º'}&nbsp;</h1>
+                      <h1 className="responses-title">&nbsp;{anime}</h1>
+                  </div>
+              </div>
+          ))}
+          
+          <button className="button back-button" onClick={routeChange}> Voltar </button>
+    </div>
+  );
+}
   
